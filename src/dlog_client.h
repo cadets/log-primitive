b4ca3857fa57c9d92166b5e552eeccd1fb51b0cf
Graeme Jenkinson <gcj21@cl.cam.ac.uk>
2019-03-25 14:55:28 +0000

    Added support for producing v1 and v2 messages.

--- src/dlog_client.h
+++ src/dlog_client.h
@@ -51,8 +51,7 @@ extern void dlog_client_close(struct dlog_handle *);
 extern int dlog_fetch(struct dlog_handle *, struct sbuf *, 
     const int32_t, const int32_t,  const int64_t, const int32_t);
 extern int dlog_list_offset(struct dlog_handle *, struct sbuf *, int64_t);
-extern int dlog_produce(struct dlog_handle *, char *,
-    unsigned char *, size_t); 
+extern int dlog_produce(struct dlog_handle *, char *, unsigned char *, size_t); 
 extern int dlog_produce_no_key(struct dlog_handle *, unsigned char *, size_t); 
 
 #endif
